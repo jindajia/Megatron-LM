161abc7cb4a26e505a03da30cd35604d64ae4967
Jinda <jiajinda001@gmail.com>
2023-09-22 11:34:03 -0400

    add saving collective data arguments

---
+++ megatron/training_info.py
@@ -0,0 +1,10 @@
+"""Training infos."""
+
+class TrainingInfo():
+
+    def __init__(self):
+        self.iteration = 0
+    def set_iteration(self, iteration):
+        self.iteration = iteration
+    def get_iteration(self):
+        return self.iteration
