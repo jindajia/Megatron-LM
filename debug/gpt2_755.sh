b814d8df17ccf7116d3ae0aeaca10ddfeb9947e3
jinda jia <62297091+jindajia@users.noreply.github.com>
2023-09-29 10:19:04 -0400

    Create gpt2_755.sh

---
+++ debug/gpt2_755.sh
@@ -0,0 +1,96 @@
+#!/bin/bash
+# Runs the "774M" parameter model
+
+module load gcc
+module load cuda
+
+mamba init
+source ~/.bashrc
+mamba activate megatron
+
+# export OMP_NUM_THREADS=2
+export TORCH_DISTRIBUTED_DEBUG=INFO
+export CUDA_DEVICE_MAX_CONNECTIONS=1
+
+GPUS_PER_NODE=1
+# Change for multinode config
+MASTER_ADDR=localhost
+MASTER_PORT=6000
+NNODES=1
+NODE_RANK=0
+WORLD_SIZE=$(($GPUS_PER_NODE*$NNODES))
+
+CHECKPOINT_PATH=/ocean/projects/asc200010p/jjia1/Developer/LLM/gpt2/checkpoint/demo
+VOCAB_FILE=/ocean/projects/asc200010p/jjia1/Developer/LLM/gpt2/vocab_file/gpt2-vocab.json
+MERGE_FILE=/ocean/projects/asc200010p/jjia1/Developer/LLM/gpt2/merge_file/gpt2-merges.txt
+DATA_PATH=/ocean/projects/asc200010p/jjia1/Developer/LLM/gpt2/datapath/gpt2_text_document
+TENSORBOARD_DIR=/ocean/projects/asc200010p/jjia1/Developer/tensorboard/gpt774M
+
+DISTRIBUTED_ARGS="
+    --nproc_per_node $GPUS_PER_NODE \
+    --nnodes $NNODES \
+    --node_rank $NODE_RANK \
+    --master_addr $MASTER_ADDR \
+    --master_port $MASTER_PORT
+"
+
+GPT_ARGS="
+    --tensor-model-parallel-size 1 \
+    --pipeline-model-parallel-size 1 \
+    --num-layers 36 \
+    --hidden-size 1280 \
+    --num-attention-heads 20 \
+    --seq-length 1024 \
+    --max-position-embeddings 1024 \
+    --micro-batch-size 4 \
+    --global-batch-size 16 \
+    --lr 0.00015 \
+    --train-iters 500000 \
+    --lr-decay-iters 320000 \
+    --lr-decay-style cosine \
+    --min-lr 1.0e-5 \
+    --weight-decay 1e-2 \
+    --lr-warmup-fraction .01 \
+    --clip-grad 1.0 \
+    --fp16 \
+    --swiglu \
+    --use-distributed-optimizer
+"
+
+DATA_ARGS="
+    --data-path $DATA_PATH \
+    --vocab-file $VOCAB_FILE \
+    --merge-file $MERGE_FILE \
+    --split 949,50,1
+"
+
+OUTPUT_ARGS="
+    --log-interval 10 \
+    --timing-log-level 2 \
+    --save-interval 10000 \
+    --eval-interval 1000 \
+    --eval-iters 10 \
+    --log-timers-to-tensorboard \
+    --tensorboard-dir ${TENSORBOARD_DIR} \
+    --tensorboard-log-interval 5 \
+    --log-memory-to-tensorboard
+"
+
+PROFILE_ARGS="
+    --profile \
+    --profile-step-start 30 \
+    --profile-step-end 32
+"
+
+PROFILE_DIR="/ocean/projects/asc200010p/jjia1/experiment_result/gpt_774M/v100-32-4"
+
+NSYS="nsys profile -s none -t nvtx,cuda --cuda-memory-usage=true -o gpt_774_profiler_data --force-overwrite true --stats=false --capture-range=cudaProfilerApi"
+
+torchrun $DISTRIBUTED_ARGS ../pretrain_gpt.py \
+    $GPT_ARGS \
+    $DATA_ARGS \
+    $OUTPUT_ARGS \
+    --distributed-backend nccl \
+    --save $CHECKPOINT_PATH \
+    --load $CHECKPOINT_PATH
+
