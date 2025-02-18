8ac0b5246abecb417d490104731c3dd72621383e
Ubuntu <ubuntu@ip-172-31-25-95.ec2.internal>
2025-02-18 19:25:43 +0000

    cuda capability

--- tools/jet_quant_cuda/setup.py
+++ tools/jet_quant_cuda/setup.py
@@ -17,6 +17,7 @@ nvcc_args = [
     '-DBF16_AVAILABLE',
     '-gencode', 'arch=compute_80,code=sm_80',
     '-gencode', 'arch=compute_86,code=sm_86',
+    '-gencode', 'arch=compute_89,code=sm_89',
     '-gencode', 'arch=compute_90,code=sm_90',
 ]
 
